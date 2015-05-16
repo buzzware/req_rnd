236c1e19860d43a77cf24ac59b5afad11994c4b7
Gary McGhee <gary@buzzware.com.au>
2015-05-16 21:16:09 +0800

    tried absolute path

--- my_app/app/main_activity.rb
+++ my_app/app/main_activity.rb
@@ -1,5 +1,16 @@
+module My
+class SomethingElse
+
+  def blahblah
+    puts 'blahblah'
+  end
+  
+end
+end
+
 class MainActivity < Android::App::Activity
   def onCreate(savedInstanceState)
+    My::SomethingElse.new.blahblah
     MyGem::Something.new.blah
     super
   end
--- my_gem/lib/my_gem.rb
+++ my_gem/lib/my_gem.rb
@@ -1,5 +1,7 @@
 require "my_gem/version"
+#require File.join(File.dirname(__FILE__),"my_gem/something")
 require "my_gem/something"
+
 # module MyGem
 #   # Your code goes here...
 # end
